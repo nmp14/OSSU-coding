191bc568fa2fd25132045eccceae66cf56844709
Nathan Peek <nmp14fsu@gmail.com>
2023-07-05 04:41:51 -0400

    Test python file

---
+++ intro-cs-and-programming-python/lec1-practice.py
@@ -0,0 +1,10 @@
+import random;
+
+nums = [1, 3, 5, 7, 9];
+
+ran = random.choice(nums);
+
+# This is a comment and I am printing a variable.
+# How not exciting this is since I know this.
+# #Javascript > python
+print(ran);
